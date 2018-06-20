9b86fca4799d857e94fbff929b712c46be6b1c49
狐狸宝宝_ <352600801@qq.com>
2018-06-20 23:37:07 +0800

    2018/06/20

--- Tsinghua Online Judge/T11.cpp
+++ Tsinghua Online Judge/T11.cpp
@@ -4,6 +4,20 @@
 #include <cmath>
 using namespace std;
 
-int main () {
+#define N 21
+long long cal[N];
 
+void init() {
+	cal[1] = 1;
+	cal[2] = 2;
+	for(int i = 3; i < N; i++) {
+		cal[i] = i * cal[i-1];
+	}
+}
+
+int main () {
+	int n;
+	init();
+	cin >> n;
+	cout << cal[n] << endl;
 }
